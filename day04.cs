f13a81d1758956dffae1c6e02d2ed6384d58c6bf
Nick Delany <nick@delany.net>
2020-12-05 22:47:41 +0000

    Day 4, Part 2

--- day04.cs
+++ day04.cs
@@ -2,6 +2,8 @@ using System;
 using System.IO;
 using System.Diagnostics;
 using System.Linq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace AdventOfCode2020
 {
@@ -36,16 +38,16 @@ namespace AdventOfCode2020
                     switch (data[0])
                     {
                         case "byr":
-                            id.byr = data[1].Trim();
+                            id.byr = int.Parse(data[1].Trim());
                             break;
                         case "iyr":
-                            id.iyr = data[1].Trim();
+                            id.iyr = int.Parse(data[1].Trim());
                             break;
                         case "eyr":
-                            id.eyr = data[1].Trim();
+                            id.eyr = int.Parse(data[1].Trim());
                             break;
                         case "hgt":
-                            id.hgt = data[1].Trim();
+                            id.hgt = new height(data[1].Trim());
                             break;
                         case "hcl":
                             id.hcl = data[1].Trim();
@@ -67,7 +69,7 @@ namespace AdventOfCode2020
 
             if (id.isValid()) valid++;
 
-            Console.WriteLine(valid);
+            Console.WriteLine("Answer: " + valid);
 
             ShowElapsed("finish", stopWatch);            
         }
@@ -79,21 +81,64 @@ namespace AdventOfCode2020
         }
     }
 
+    public class height
+    {
+        public Boolean v;
+
+        Regex rxHgt = new Regex(@"(\d+)(cm|in)",
+          RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        public height(string d)
+        {
+            var mg = rxHgt.Matches(d);
+
+            if (mg.Count != 1)
+            {
+                v = false;
+                return;
+            }
+
+            var m = mg[0].Groups;
+            var h = int.Parse(m[1].Value);
+            if (m[2].Value == "cm")
+            {
+                v = (h <= 193 && h >= 150);
+            }
+            else
+                v = (h <= 76 && h >= 59);
+        }
+    }
+
     public class id 
     {
-        public string byr;
-        public string iyr;
-        public string eyr;
-        public string hgt;
+        public int? byr;
+        public int? iyr;
+        public int? eyr;
+        public height hgt;
         public string hcl;
         public string ecl;
         public string pid;
         public string cid;
 
+        Regex rxHcl = new Regex(@"#[0-9a-f]{6}",
+          RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        Regex rxPid = new Regex(@"\d{9}",
+          RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        List<string> rxEcl = new List<string>{ "amb", "blu","brn","gry","grn","hzl","oth" };
+
         public Boolean isValid()
         {
-            return byr != null &&
-            iyr != null && eyr != null && hgt != null && hcl != null && ecl != null && pid != null;
+            return 
+                (byr != null && byr <= 2002 && byr >= 1920)
+                && (iyr != null && iyr <= 2020 && iyr >= 2010) 
+                && (eyr != null && eyr <= 2030 && eyr >= 2020) 
+                && (hgt != null && hgt.v) 
+                && (hcl != null && hcl.Length == 7 && rxHcl.IsMatch(hcl)) 
+                && (ecl != null && rxEcl.Contains(ecl)) 
+                && (pid != null && pid.Length == 9 && rxPid.IsMatch(pid))
+                ;
         }
     }
 }
