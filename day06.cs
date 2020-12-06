9682cbffb7583f185c3c61d671ef59f77dce24ac
Nick Delany <nick@delany.net>
2020-12-06 20:01:20 +0000

    Day 6, part 2

--- day06.cs
+++ day06.cs
@@ -16,25 +16,26 @@ namespace AdventOfCode2020
             var contents = System.IO.File.ReadAllLines("input/day06.data");
 
             int[] data = new int[contents.Length];
-            int ix = 0;
-            Boolean [] chk = new Boolean[26];
+            int ix = 0, head = 0;
+            int [] chk = new int[26];
 
             foreach (var line in contents)
             {
                 if (String.IsNullOrEmpty(line))
                 {
-                    data[ix] = chk.Count(x => x);
+                    data[ix] = chk.Count(x => x == head);
                     ix++;
-                    for (var i = 0; i < 26; i++) chk[i] = false;
+                    for (var i = 0; i < 26; i++) chk[i] = 0;
+                    head = 0;
+                    continue;
                 }
 
                 foreach (var c in line)
-                    chk[c - 97] = true;
-
+                    chk[c - 97]++;
+                head++;
             }
 
-            data[ix] = chk.Count(x => x);
-            ix++;
+            data[ix] = chk.Count(x => x == head);
 
             Console.WriteLine("Answer: " + data.Sum());
 
